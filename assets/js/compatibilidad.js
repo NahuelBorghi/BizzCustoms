db4d8c040ceca06bcc3482d5a42a70a7a57dcb0c
nahuel borghi <nahuelborghi@gmail.com>
2021-04-04 19:29:16 -0300

    comienzo de algoritmo de compatibilidad entre componentes y calculo de consumo de watts

--- assets/js/compatibilidad.js
+++ assets/js/compatibilidad.js
@@ -1,22 +1,13 @@
-class motherboard {
-    constructor (socalo,nombre,video,ram){
-        this.socalo = socalo;
-        this.nombre = nombre;
-        this.video = video;
-        this.ram = ram;
+class compat {
+    constructor (){
+        
     }
-    alerta (){
-        alert("la placa madre solo es compatible con procesadores "+this.socalo);
-        alert("podes poner hasta "+this.ram+" modulos de ram");
-    }
+    conclusion (){ //generar html para #conclusion
 
+    }
 }
+//poner efecto de profundidad en css a los input y/o los divs
 
 function nuevo(){
-    var socalo = document.getElementById("socalo").value;
-    var nombre = document.getElementById("nombre").value;
-    var video = document.formMother.video.value;
-    var ram = parseInt(document.formMother.ram.value);
-    var mother = new motherboard(socalo,nombre,video,ram);
-    mother.alerta();
+    
 }
